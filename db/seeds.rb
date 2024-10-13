76f851a9243c1bcdce2b51e0d6facd5059a2e787
ZephyRemix <hunyaochong@gmail.com>
2024-10-13 10:29:01 +0800

    Added association between flight and airport, and seed data for both

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,32 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+# iata_codes = [
+#   "ATL", "PEK", "LAX", "DXB", "HND",
+#   "ORD", "LHR", "CDG", "AMS", "FRA",
+#   "SYD", "JFK", "SIN", "YYZ", "MEX",
+#   "ICN", "GRU", "BKK", "MUC", "HKG"
+# ]
+
+# iata_codes.each do |c|
+#   Airport.create(code: c)
+# end
+
+# 50.times do
+#   airport_id = Airport.pluck(:id)
+
+#   d_id = airport_id.sample
+#   a_id = airport_id.sample
+#   while d_id == a_id
+#     a_id = airport_id.sample
+#   end
+
+#   Flight.create(
+#     departure_airport_id: d_id,
+#     arrival_airport_id: a_id,
+#     date: Faker::Date.between(from: Date.today, to: 1.year.from_now),
+#     duration_minutes: Faker::Number.within(range: 30..100)
+#   )
+# end
